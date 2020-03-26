667eb1e00918bf84a870ee74b9d3dc54a83cca3b
Maciej Majchrowski <maciek21197@gmail.com>
2020-03-26 14:20:23 +0100

    added 3 criteria

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,49 @@
 from pandas import read_csv
 from app.configuration.config import json_config
 
+def minMaxCriteria(dataSet, labelColumn):
+    number = dataset.loc[:, dataset.columns != labelColumn]
+    minimumValuesInRows = dataset.min(axis=1)
+    indexOfMaximumOfMinimum = minimumValuesInRows.idxmax()
+    minMaxCriteriaValue = dataset.loc[indexOfMaximumOfMinimum, dataset.columns == labelColumn]
+    return minMaxCriteriaValue
+
+def optimisicCriteria(dataSet, labelColumn):
+    number = dataset.loc[:, dataset.columns != labelColumn]
+    maximumValuesInRows = dataset.max(axis=1)
+    indexOfMaximum = maximumValuesInRows.idxmax()
+    maxCriteriaValue = dataset.loc[indexOfMaximum, dataset.columns == labelColumn]
+    return maxCriteriaValue
+
+def huwriczCriteria(dataSet, labelColumn, cautionFactor):
+    number = dataset.loc[:, dataset.columns != labelColumn]
+    minimumValuesInRows = dataset.min(axis=1)
+    maximumValuesInRows = dataset.max(axis=1)
+    maxHuwricz = 0
+    indexOfRow = 0
+    bestRow = 0
+    for i, (minValue, maxValue) in enumerate(zip(minimumValuesInRows, maximumValuesInRows)):
+        huwriczValueInRow = cautionFactor * minValue + (1 - cautionFactor) * maxValue
+        if maxHuwricz < huwriczValueInRow:
+            maxHuwricz = huwriczValueInRow
+            bestRow = i
+
+    # for x in range(len(dataSet.index)) :
+    #     print(len(dataSet.index))
+    #     huwriczValueInRow = cautionFactor * minimumValuesInRows[x] + (1 - cautionFactor) * maximumValuesInRows[x]
+    #     if maxHuwricz < huwriczValueInRow:
+    #         maxHuwricz = huwriczValueInRow
+    #         bestRow = x
+    
+    huwriczCriteriaValue = dataset.loc[bestRow, dataset.columns == labelColumn]
+    return huwriczCriteriaValue
+
 print(json_config.dataSourceUrl)
 dataset = read_csv(json_config.dataSourceUrl)
 
-print(dataset)
+print("-----------Kryterium Huwricza-----------")
+print(huwriczCriteria(dataSet=dataset, labelColumn=json_config.labelColumn, cautionFactor=json_config.cautionFactor))
+print("-----------Kryterium Optymistyczne-----------")
+print(optimisicCriteria(dataSet=dataset, labelColumn=json_config.labelColumn))
+print("-----------Kryterium Walda-----------")
+print(minMaxCriteria(dataSet=dataset, labelColumn=json_config.labelColumn))
--- app/configuration/config.py
+++ app/configuration/config.py
@@ -2,14 +2,17 @@ from pathlib import Path
 from pandas import read_json
 
 class Config(object):
-    def __init__(self, dataSourceUrl):
+    def __init__(self, dataSourceUrl, labelColumn, cautionFactor):
         self.dataSourceUrl = dataSourceUrl
-
+        self.labelColumn = labelColumn
+        self.cautionFactor = cautionFactor
+        
 def as_config(dict):
     dataSetName = dict['chosenDataSet'][0]
-    print(dict[dataSetName])
     return Config(
-        dict[dataSetName]['dataSourceUrl']
+        dict[dataSetName]['dataSourceUrl'],
+        dict[dataSetName]['labelColumn'],
+        dict[dataSetName]['cautionFactor'],
     )
 
 # configuration = json.loads(Path("configuration.json").read_text(), object_hook = as_config)
