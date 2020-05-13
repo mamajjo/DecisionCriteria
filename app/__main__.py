0d4d8e8e0e333ac294f9bccb7991ac32dcc84760
Jakub Kurek <jakub.kurek@goodylabs.com>
2020-05-13 10:13:09 +0200

    Initial exer4

--- app/__main__.py
+++ app/__main__.py
@@ -1,59 +1,67 @@
 import numpy as np
 from app.configuration.config import configuration
-from collections import defaultdict 
+from collections import defaultdict
 
-class Graph: 
+
+class Graph:
     def __init__(self, values=[]):
         self.V = values
 
-    def minDistance(self,dist,queue): 
-        minim = float("Inf") 
-        min_index = -1
-        for i in range(len(dist)): 
-            if dist[i] < minim and i in queue: 
-                minim = dist[i] 
-                min_index = i 
-        return min_index 
-    def find_shortest_paths(self, start): 
-        row = len(self.V) 
-        col = len(self.V[0]) 
-        dist = [float("Inf")] * row 
-        parent = [-1] * row 
-        dist[start] = 0
+    def shortestDist(self):
+        graph = self.V
+        global INF
 
-        queue = [] 
-        for i in range(row): 
-            queue.append(i) 
+        # dist[i] is going to store shortest
+        # distance from node i to node N-1.
+        dist = [0] * N
 
-        while queue: 
-            u = self.minDistance(dist,queue)  
-            queue.remove(u) 
-            for i in range(col): 
-                if self.V[u][i] and i in queue: 
-                    if dist[u] + self.V[u][i] < dist[i]: 
-                        dist[i] = dist[u] + self.V[u][i] 
-                        parent[i] = u 
+        dist[N - 1] = 0
 
-        self.print_paths(dist, parent, start)
-    def print_path(self, parent, j): 
-        if parent[j] == -1:  
-            print (f"{j}", end='')
-            return
-        self.print_path(parent , parent[j]) 
-        print (f" -> {j}", end='')
-    def print_paths(self, dist, parent, start): 
-        print("Vertice_num \t\tmin_distance \t path", end='') 
-        for i in range(1, len(dist)): 
-            print("\n%d --> %d \t\t%d \t\t" % (start, i, dist[i]), end='')
-            self.print_path(parent,i) 
+        # Calculating shortest path
+        # for rest of the nodes
+        for i in range(N - 2, -1, -1):
+
+            # Initialize distance from
+            # i to destination (N-1)
+            dist[i] = INF
+
+            # Check all nodes of next stages
+            # to find shortest distance from
+            # i to N-1.
+            for j in range(N):
+
+                # Reject if no edge exists
+                if graph[i][j] == INF:
+                    continue
+
+                # We apply recursive equation to
+                # distance to target through j.
+                # and compare with minimum
+                # distance so far.
+                dist[i] = min(dist[i],
+                              graph[i][j] + dist[j])
+
+        return dist[0]
+
+
+N = 9
+INF = 99
+
+# rawGraph = [[INF, 1, 2, 5, INF, INF, INF, INF],
+#             [INF, INF, INF, INF, 4, 11, INF, INF],
+#             [INF, INF, INF, INF, 9, 5, 16, INF],
+#             [INF, INF, INF, INF, INF, INF, 2, INF],
+#             [INF, INF, INF, INF, INF, INF, INF, 18],
+#             [INF, INF, INF, INF, INF, INF, INF, 13],
+#             [INF, INF, INF, INF, INF, INF, INF, 2]]
 
 rawGraph = np.loadtxt(configuration.graphPath, dtype='i', delimiter=' ')
 try:
-    if(len(rawGraph) < 0):
+    if (len(rawGraph) < 0):
         raise AttributeError(f"At least one row of data is required")
-    g= Graph(rawGraph) 
+    g = Graph(rawGraph)
     # indexing from 0
-    g.find_shortest_paths(0) 
+    print(g.shortestDist())
 except AttributeError as error:
     print('in configuration file: ' + repr(error))
 except KeyError as keyError:
