fd9374d12a3f6fe62e4ba27ab4383f4e714b7682
Mateusz <210183@edu.p.lodz.pl>
2020-05-13 17:04:23 +0200

    code for exer4

--- app/__main__.py
+++ app/__main__.py
@@ -1,15 +1,14 @@
 import numpy as np
 from app.configuration.config import configuration
 INT_MAX = 999999
-def mindist(GRAPH, source, dst, checked, parent):
-    #Base Conditions
-    if source == dst:
+def calc_dist(GRAPH, source, destination, checked, parent):
+    if source == destination:
         return 0
     checked[source] = True
     min_dist = INT_MAX
     for k in range(V):
         if checked[k] == False and GRAPH[source][k]:
-            res = mindist(GRAPH, k, dst, checked, parent)
+            res = calc_dist(GRAPH, k, destination, checked, parent)
             if res != INT_MAX:
                 if min_dist > GRAPH[source][k] + res:
                     min_dist = GRAPH[source][k] + res
@@ -17,11 +16,11 @@ def mindist(GRAPH, source, dst, checked, parent):
     checked[source] = False
     return min_dist
 
-def printpath(parent, dst):
-    if parent[dst] == -1:
+def show_sol(parent, destination):
+    if parent[destination] == -1:
         return
-    printpath(parent, int(parent[dst]))
-    print(f"{int(parent[dst])} --> ", end='')
+    show_sol(parent, int(parent[destination]))
+    print(f"{int(parent[destination])} --> ", end='')
 
 rawGraph = np.loadtxt(configuration.graphPath, dtype='i', delimiter=' ')
 V = rawGraph.shape[0]
@@ -30,14 +29,14 @@ parent = np.zeros(V)
 for i in range(V):
     checked[i] = False
     parent[i] = -1
-print(V)
+print(f"Size of adjacency matrix is {V}")
 try:
     if (len(rawGraph) < 0):
         raise AttributeError(f"At least one row of data is required")
     g = rawGraph
-    print(f" Min Distance from 0 to {V-1} : {mindist(g, 0, V-1, checked, parent)}")
+    print(f" Min Distance from 0 to {V-1} : {calc_dist(g, 0, V-1, checked, parent)}")
     print(f"Path: ", end='')
-    printpath(parent, V-1)
+    show_sol(parent, V-1)
     print(V-1, end='')
 except AttributeError as error:
     print('in configuration file: ' + repr(error))
